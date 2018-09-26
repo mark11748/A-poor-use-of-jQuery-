a54eabdf615648692dd7bb46b2b11201540e5684
Mark Woodward <markwood117@gmail.com>
2018-09-26 14:42:03 -0700

    added bg-color selector buttons;

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,39 @@
-$(function() {
+$(document).ready(function() {
   $('div.card-body').click(function() {
     $('img').remove('#BAIT');
-    $('.card-body').append('<iframe id="REACTION" width="100%" src="https://www.youtube.com/embed/CO7zNaRUC7M?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+    $('.card-body').append('<iframe id="REACTION" width="100%" height="360px" src="https://www.youtube.com/embed/CO7zNaRUC7M?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+  });
+  $('button#GREEN').click(function() {
+    if($('*:not(.card) + *:not(.card>*)').hasClass('green-bg'))
+    {
+      $('*:not(.card) + *:not(.card>*)').removeClass('green-bg');
+    }
+    else {
+      $('*:not(.card) + *:not(.card>*)').removeClass('blue-bg');
+      $('*:not(.card) + *:not(.card>*)').removeClass('red-bg');
+      $('*:not(.card) + *:not(.card>*)').addClass('green-bg');
+    }
+  });
+  $('button#RED').click(function() {
+    if($('*:not(.card) + *:not(.card>*)').hasClass('red-bg'))
+    {
+      $('*:not(.card) + *:not(.card>*)').removeClass('red-bg');
+    }
+    else {
+      $('*:not(.card) + *:not(.card>*)').removeClass('blue-bg');
+      $('*:not(.card) + *:not(.card>*)').removeClass('green-bg');
+      $('*:not(.card) + *:not(.card>*)').addClass('red-bg');
+    }
+  });
+  $('button#BLUE').click(function() {
+    if($('*:not(.card) + *:not(.card>*)').hasClass('blue-bg'))
+    {
+      $('*:not(.card) + *:not(.card>*)').removeClass('blue-bg');
+    }
+    else {
+      $('*:not(.card) + *:not(.card>*)').removeClass('green-bg');
+      $('*:not(.card) + *:not(.card>*)').removeClass('red-bg');
+      $('*:not(.card) + *:not(.card>*)').addClass('blue-bg');
+    }
   });
 });
